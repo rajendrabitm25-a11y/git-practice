faed520ce251aa0a293e2bb6811808b4a07ea118
[Rajendra Jabara] <rajendrabitm25@oic.edu.np>
2026-03-20 17:29:30 +0545

    added another image

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
 void main()  {
-    printf("Hello world");
+    printf("Enter any number:  \n");
+    int n;
+    scanf("%d", &n);
+    if (n % 2 == 0) {
+        printf("%d is Even Number", n);
+    } else {
+        printf("%d is Odd Number", n);
+    }
 }
